8b637435d118eb76831236f1271d2b416fe0d391
Ottomata <ottomata@proton.me>
2024-11-14 23:50:44 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -85,7 +85,7 @@ char	*read_file(int fd, char *res)
 		}
 		buffer[byte_read] = 0;
 		res = ft_specialjoin(res, buffer);
-		if (ft_char_in_s(buffer, '\n') >= 0)
+		if (ft_indexof(buffer, '\n') >= 0)
 			break ;
 	}
 	free(buffer);
